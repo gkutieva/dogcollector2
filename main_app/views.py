7cab14324c057e93d030d3933f9fe9c8e5cd3853
gkutieva <73370477+gkutieva@users.noreply.github.com>
2020-12-17 15:01:34 -0700

    Register Admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
+from .models import Dog
 
 # Create your views here.
 from django.http import HttpResponse
-
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-dogs = [
-    Dog('Molly', 'husky', 'can speak small words', 5),
-    Dog('Coco', 'maltese', 'fearless in a charming toy-dog way', 3),
-    Dog('Axel', 'pug', 'stable temperament and great charm', 7),
-    Dog('Cesar', 'long-hair chihuahua', 'hunt rats and other small animals', 10)
-]
 
 # Define our home view
 def home(request):
@@ -25,4 +12,5 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
