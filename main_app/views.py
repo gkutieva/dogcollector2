edc1bea583d0122b5420092488c5f98ece597822
gkutieva <73370477+gkutieva@users.noreply.github.com>
2020-12-21 14:43:34 -0500

    added more html

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,73 @@
-from django.shortcuts import render
-from .models import Dog
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
+from .forms import FeedingForm
 
-# Create your views here.
-from django.http import HttpResponse
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'description', 'age']
 
-# Define our home view
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
 def home(request):
-    return HttpResponse('<h1>Hello Dog Lovers ໒(＾ᴥ＾)७</h1>')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def dogs_index(request):
-    dogs = Dog.objects.all()
-    return render(request, 'dogs/index.html', {'dogs': dogs})
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
 
 def dogs_detail(request, dog_id):
-    dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dogs/detail.html', { 'dog': dog })
+  dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+  # instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {
+    # pass the cat and feeding_form as context
+    'dog': dog, 'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have
+  })
+
+def add_feeding(request, dog_id):
+	# create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
+def assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
