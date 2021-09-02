9d9567654e9726bc3dd61c39b19a2b42204a9ffc
gkutieva <73370477+gkutieva@users.noreply.github.com>
2021-09-02 15:33:01 -0700

    added model for pictures

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Dog, Toy
+from .models import Dog, Toy, Photo
 from .forms import FeedingForm
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3.us-west-1.amazonaws.com/'
+BUCKET = 'dogcollector2-gk'
 
 class DogCreate(CreateView):
   model = Dog
@@ -48,6 +53,24 @@ def add_feeding(request, dog_id):
     new_feeding.dog_id = dog_id
     new_feeding.save()
   return redirect('detail', dog_id=dog_id)
+
+def add_photo(request, dog_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            Photo.objects.create(url=url, dog_id=dog_id)
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', dog_id=dog_id)
 
 def assoc_toy(request, dog_id, toy_id):
   # Note that you can pass a toy's id instead of the whole object
